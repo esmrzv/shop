af42b490d7612e51103fe2dc504f81137db28824
esmrzv <esmurziev.1999@mail.ru>
2024-06-15 18:55:10 +0300

    готовая

--- catalog/templatetags/my_tags.py
+++ catalog/templatetags/my_tags.py
@@ -6,12 +6,6 @@ register = template.Library()
 @register.filter()
 def media_filter(path):
     if path:
-        return f'media/{path}'
+        return f'/media/{path}'
     return '#'
 
-
-# @register.filter()
-# def truncatechars(path):
-#     if len(path) >= 100:
-#         return path[0:99]
-#     return path
